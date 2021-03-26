db7419f7b6f2f922453eb08ce83cab1df1e6ec40
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-03-26 09:55:04 -0300

    :white_check_mark: Resolvido o requisito 20 -  último requisito

--- script.js
+++ script.js
@@ -9,16 +9,33 @@ btnEnviar.addEventListener('click', alertEmail);
 
 // 18°
 const getButton = document.getElementById('facebook-register');
-getButton.addEventListener('click', () => {
+
+function save() {
+  const getPai = document.querySelector('.right-content');
+  const getFilho2 = document.querySelector('.formContato');
+  getPai.removeChild(getFilho2);
+  // adicionando
+  const addDiv = document.createElement('div');
+  const storage = sessionStorage.getItem('teste1');
+  const storage2 = sessionStorage.getItem('teste2');
+  addDiv.className = 'teste5';
+  getPai.appendChild(addDiv);
+  const getDiv5 = document.querySelector('.teste5');
+  getDiv5.innerHTML = storage + storage2;
+}
+
+function camposInvalidos() {
   const getFilho = document.querySelector('.formContato').children;
   const getCamposInvalidos = document.getElementById('camposInvalidos');
-  for (let i = 1; i < getFilho.length; i += 1) {
+  for (let i = 0; i < getFilho.length; i += 1) {
     if (getFilho[i].value === '') {
       getCamposInvalidos.innerHTML = 'Campos inválidos';
     }
   }
-});
+  getButton.addEventListener('click', save);
+}
 
+getButton.addEventListener('click', camposInvalidos);
 // 19
 const genderOptional = document.createElement('input');
 genderOptional.name = 'gender-custom';
@@ -49,7 +66,6 @@ maleRadio.addEventListener('click', () => {
 });
 // 20°
 // capturar informação
-
 const getName = document.getElementById('firstname');
 const getLastName = document.getElementById('lastname');
 const getEmail = document.getElementById('phone_email');
@@ -62,27 +78,13 @@ getButton.addEventListener('click', () => {
   if (getName.value && getLastName.value && getEmail.value && getData.value) {
     const teste = (`Olá, ${getName.value} ${getLastName.value}
     ${getEmail.value} ${getData.value} `);
-    localStorage.setItem('teste1', teste);
+    sessionStorage.setItem('teste1', teste);
   }
 });
 getButton.addEventListener('click', () => {
   for (let index = 0; index < getSexChildren.length; index += 1) {
     if (getSexChildren[index].checked === true) {
-      localStorage.setItem('teste2', getSexChildren[index].value);
+      sessionStorage.setItem('teste2', getSexChildren[index].value);
     }
   }
-});
-// remover
-getButton.addEventListener('click', () => {
-  const getPai = document.getElementsByClassName('right-content')[0];
-  const getFilho2 = document.getElementsByClassName('formContato')[0];
-  getPai.removeChild(getFilho2);
-  // adicionando
-  const addDiv = document.createElement('div');
-  const storage = localStorage.getItem('teste1');
-  const storage2 = localStorage.getItem('teste2');
-  addDiv.className = 'teste5';
-  getPai.appendChild(addDiv);
-  const getDiv5 = document.querySelector('.teste5');
-  getDiv5.innerHTML = storage + storage2;
 });
