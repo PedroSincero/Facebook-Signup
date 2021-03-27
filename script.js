00516f71863534c2162af79ef3323284d005991c
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-03-27 16:14:53 -0300

    Testando a resolução do requisito 20

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const inputEmail = document.querySelector('#user-email-phone');
 const btnEnviar = document.querySelector('#button-login');
+const nome = sessionStorage.getItem('nome');
+const sobrenome = sessionStorage.getItem('sobrenome');
+const email = sessionStorage.getItem('email');
+const data = sessionStorage.getItem('data');
+const genero = sessionStorage.getItem('genero');
+const h1 = document.createElement('h1');
+const h3 = document.createElement('h3');
+const p = document.createElement('p');
+const p2 = document.createElement('p');
 
 function alertEmail() {
   alert(inputEmail.value);
@@ -10,29 +19,35 @@ btnEnviar.addEventListener('click', alertEmail);
 // 18°
 const getButton = document.getElementById('facebook-register');
 
-function save() {
+function insertOnForm() {
   const getPai = document.querySelector('.right-content');
   const getFilho2 = document.querySelector('.formContato');
   getPai.removeChild(getFilho2);
-  // adicionando
   const addDiv = document.createElement('div');
-  const storage = sessionStorage.getItem('teste1');
-  const storage2 = sessionStorage.getItem('teste2');
   addDiv.className = 'teste5';
   getPai.appendChild(addDiv);
   const getDiv5 = document.querySelector('.teste5');
-  getDiv5.innerHTML = storage + storage2;
+  getDiv5.appendChild(h1).innerHTML = `Olá, ${nome} ${sobrenome}`;
+  getDiv5.appendChild(h3).innerHTML = `${email}`;
+  getDiv5.appendChild(p).innerHTML = `${data}`;
+  getDiv5.appendChild(p2).innerHTML = `${genero}`;
 }
-
+const getFormPai = document.querySelector('.formContato');
+const getFormFilho = getFormPai.getElementsByTagName('input');
 function camposInvalidos() {
-  const getFilho = document.querySelector('.formContato').children;
   const getCamposInvalidos = document.getElementById('camposInvalidos');
-  for (let i = 1; i < getFilho.length; i += 1) {
-    if (getFilho[i].value === '') {
-      getCamposInvalidos.innerHTML = 'Campos inválidos';
+  for (let i = 0; i < getFormFilho.length; i += 1) {
+    if (getFormFilho[i].value === '') {
+      getCamposInvalidos.innerText = 'Campos inválidos';
+      getFormFilho[i].addEventListener('keydown', () => {
+        getCamposInvalidos.innerText = '';
+      });
+      break;
     }
   }
-  getButton.addEventListener('click', save);
+  if (getCamposInvalidos.innerText !== 'Campos inválidos') {
+    document.addEventListener('submit', insertOnForm);
+  }
 }
 
 getButton.addEventListener('click', camposInvalidos);
@@ -76,15 +91,20 @@ const getSexChildren = getSex.children;
 
 getButton.addEventListener('click', () => {
   if (getName.value && getLastName.value && getEmail.value && getData.value) {
-    const teste = (`Olá, ${getName.value} ${getLastName.value}
-    ${getEmail.value} ${getData.value} `);
-    sessionStorage.setItem('teste1', teste);
+    const nome2 = getName.value;
+    const sobrenome2 = getLastName.value;
+    const email2 = getEmail.value;
+    const data2 = getData.value;
+    sessionStorage.setItem('nome', nome2);
+    sessionStorage.setItem('sobrenome', sobrenome2);
+    sessionStorage.setItem('email', email2);
+    sessionStorage.setItem('data', data2);
   }
 });
 getButton.addEventListener('click', () => {
   for (let index = 0; index < getSexChildren.length; index += 1) {
     if (getSexChildren[index].checked === true) {
-      sessionStorage.setItem('teste2', getSexChildren[index].value);
+      sessionStorage.setItem('genero', getSexChildren[index].value);
     }
   }
 });
