f6f463e23d372433c12e80410dcc8e87f8a432ad
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-03-29 06:30:03 -0300

    :white_check_mark: Resolvido o Requisito 20 - Vizualisar os dados na tela

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const inputEmail = document.querySelector('#user-email-phone');
 const btnEnviar = document.querySelector('#button-login');
-const nome = sessionStorage.getItem('nome');
-const sobrenome = sessionStorage.getItem('sobrenome');
-const email = sessionStorage.getItem('email');
-const data = sessionStorage.getItem('data');
-const genero = sessionStorage.getItem('genero');
 const h1 = document.createElement('h1');
 const h3 = document.createElement('h3');
 const p = document.createElement('p');
 const p2 = document.createElement('p');
+const getName = document.getElementById('firstname');
+const getLastN = document.getElementById('lastname');
+const getEmail = document.getElementById('phone_email');
+const getData = document.getElementById('birthdate');
 
 function alertEmail() {
   alert(inputEmail.value);
@@ -16,10 +15,10 @@ function alertEmail() {
 
 btnEnviar.addEventListener('click', alertEmail);
 
-// 18°
 const getButton = document.getElementById('facebook-register');
 
 function insertOnForm() {
+  const getSex = document.querySelector('input[type="radio"]:checked').value;
   const getPai = document.querySelector('.right-content');
   const getFilho2 = document.querySelector('.formContato');
   getPai.removeChild(getFilho2);
@@ -27,13 +26,15 @@ function insertOnForm() {
   addDiv.className = 'teste5';
   getPai.appendChild(addDiv);
   const getDiv5 = document.querySelector('.teste5');
-  getDiv5.appendChild(h1).innerHTML = `Olá, ${nome} ${sobrenome}`;
-  getDiv5.appendChild(h3).innerHTML = `${email}`;
-  getDiv5.appendChild(p).innerHTML = `${data}`;
-  getDiv5.appendChild(p2).innerHTML = `${genero}`;
+  getDiv5.appendChild(h1).innerHTML = `Olá, ${getName.value} ${getLastN.value}`;
+  getDiv5.appendChild(h3).innerHTML = `${getEmail.value}`;
+  getDiv5.appendChild(p).innerHTML = `${getData.value}`;
+  getDiv5.appendChild(p2).innerHTML = `${getSex}`;
 }
+
 const getFormPai = document.querySelector('.formContato');
 const getFormFilho = getFormPai.getElementsByTagName('input');
+
 function camposInvalidos() {
   const getCamposInvalidos = document.getElementById('camposInvalidos');
   for (let i = 0; i < getFormFilho.length; i += 1) {
@@ -51,7 +52,7 @@ function camposInvalidos() {
 }
 
 getButton.addEventListener('click', camposInvalidos);
-// 19
+
 const genderOptional = document.createElement('input');
 genderOptional.name = 'gender-custom';
 genderOptional.placeholder = 'Gênero (opcional)';
@@ -77,34 +78,5 @@ maleRadio.addEventListener('click', () => {
   const elemento2 = document.querySelector('.selected');
   if (elemento2) {
     elemento2.parentNode.removeChild(elemento2);
-  }
-});
-// 20°
-// capturar informação
-const getName = document.getElementById('firstname');
-const getLastName = document.getElementById('lastname');
-const getEmail = document.getElementById('phone_email');
-const getData = document.getElementById('birthdate');
-// const getForm = document.getElementsByClassName(' ');
-const getSex = document.querySelector('.genders');
-const getSexChildren = getSex.children;
-
-getButton.addEventListener('click', () => {
-  if (getName.value && getLastName.value && getEmail.value && getData.value) {
-    const nome2 = getName.value;
-    const sobrenome2 = getLastName.value;
-    const email2 = getEmail.value;
-    const data2 = getData.value;
-    sessionStorage.setItem('nome', nome2);
-    sessionStorage.setItem('sobrenome', sobrenome2);
-    sessionStorage.setItem('email', email2);
-    sessionStorage.setItem('data', data2);
-  }
-});
-getButton.addEventListener('click', () => {
-  for (let index = 0; index < getSexChildren.length; index += 1) {
-    if (getSexChildren[index].checked === true) {
-      sessionStorage.setItem('genero', getSexChildren[index].value);
-    }
   }
 });
